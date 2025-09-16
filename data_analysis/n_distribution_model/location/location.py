ae3142aeaf64c2e949e1852adf7472aeae6046f0
Anodymous1 <andy.co.yu@gmail.com>
2025-09-16 10:57:46 +0200

    changed location algorithm

--- data_analysis/n_distribution_model/location/location.py
+++ data_analysis/n_distribution_model/location/location.py
@@ -14,40 +14,65 @@ class locator:
     
 
     #Clean Data and find Length of csv
-    def findLength(self):
+    # def findLength(self):
 
-        time = 1000
-        self.frequency = []
+    #     time = 1000
+    #     self.frequency = []
+
+    #     for df in self.record:
+
+    #         df = df[df["time"] <= time] 
+
+    #         self.frequency.append(df.shape[0])
+
+
+    # def findMaxFrequency(self):
+    #     #Find the maximum
+    #     self.max = max(self.frequency)
+    #     self.max_id = self.frequency.index(self.max)
+
+    # def printMax(self):
+    #     self.readcsv()
+    #     self.findLength()
+    #     self.findMax()
+
+
+    #     print(f"max = {self.max}, datapoint = {self.max_id}")
+    #     print(f"Position: {self.dfs[self.max_id]}")
+    #     print(self.dfs)
+
+
+    def findMaxRate(self):
+        self.max_rate = max(self.rates)
+        self.max_rate_id = self.rates.index(self.max_rate)
+
+    def getRate(self):
+        self.readcsv()
+
+
+        self.rates = []
 
         for df in self.record:
+            hits = df["trigger_data"].sum()
 
-            df = df[df["time"] < time] 
-            
-            self.frequency.append(df.shape[0])
+            rate = hits / df.shape[0]
 
-
-    def findMax(self):
-        #Find the maximum
-        self.max = max(self.frequency)
-        self.max_id = self.frequency.index(self.max)
-
-    def output(self):
-        self.readcsv()
-        self.findLength()
-        self.findMax()
-
-
-        print(f"max = {self.max}, datapoint = {self.max_id}")
-        print(f"Position: {self.dfs[self.max_id]}")
+            self.rates.append(rate)
+        
+        
+        self.findMaxRate()
+        
+        print(f"max rate: {self.max_rate}, datapoint = {self.max_rate_id}")
+        print(f"Position: {self.dfs[self.max_rate_id]}")
         print(self.dfs)
 
 
 
 files = [
-    ""
+
 
 ]
 
 x = locator(files)
 
-x.output()
+x.getRate()
